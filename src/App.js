a0781d66479c27d2eb93806219a2823da1ac6388
Huy <trannhathuy1305@gmail.com>
2023-03-24 21:53:15 +0700

    create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
- return <div>App</div>;
-};
+import {
+ Navbar,
+ Feed,
+ VideoDetail,
+ ChannelDetail,
+ SearchFeed,
+} from './components';
+
+const App = () => (
+ <BrowserRouter>
+  <Box sx={{ background: '#000' }}>
+   <Navbar />
+   <Routes>
+    <Route path="/" exact element={<Feed />} />
+    <Route path="/video/:id" element={<VideoDetail />} />
+    <Route path="/channel/:id" element={<ChannelDetail />} />
+    <Route path="/search/:searchTerm" element={<SearchFeed />} />
+   </Routes>
+  </Box>
+ </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Stack } from '@mui/system';
+import { Link } from 'react-router-dom';
+import { logo } from './utils/contants';
+
+const Navbar = () => {
+ return <div>Navbar</div>;
+};
+
+export default Navbar;
